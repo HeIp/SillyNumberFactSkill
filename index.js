cb3aa45a61e0af4f88e1e67d678f38107241c10e
Help <pczerokey@gmail.com>
2018-11-04 12:44:47 -0500

    Cleaned up syntax.
    Changes:
    braces on their own line,
    space before declaration clean up,
    indentation spacing modified.

--- index.js
+++ index.js
@@ -1,85 +1,95 @@
 /* eslint-disable  func-names */
 /* eslint-disable  no-console */
-
 const Alexa = require('ask-sdk');
 const http = require(`http`);
 
 const GetNewFactHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'LaunchRequest'
-      || (request.type === 'IntentRequest'
-        && request.intent.name === 'GetNewFactIntent');
-  },
-  handle(handlerInput) {
-    let options = {
-      host: `numbersapi.com`,
-      port: 80,
-      path: `/random/trivia`,
-      method: `GET`,
-    };
-    async function buildAlexaResponse(options){
-      let responseFromAPI = await httpRequestPromise(options);
-      return handlerInput.responseBuilder
-      .speak(GET_FACT_MESSAGE+responseFromAPI)
-      .getResponse();
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'LaunchRequest' ||
+            (request.type === 'IntentRequest' &&
+                request.intent.name === 'GetNewFactIntent');
+    },
+    handle(handlerInput)
+    {
+        let options = {
+            host: `numbersapi.com`,
+            port: 80,
+            path: `/random/trivia`,
+            method: `GET`,
+        };
+        async function buildAlexaResponse(options)
+        {
+            let responseFromAPI = await httpRequestPromise(options);
+            return handlerInput.responseBuilder
+                .speak(GET_FACT_MESSAGE + responseFromAPI)
+                .getResponse();
+        }
+        return buildAlexaResponse(options);
     }
-    return buildAlexaResponse(options);
-  }
 };
 
 const HelpHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && request.intent.name === 'AMAZON.HelpIntent';
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(HELP_MESSAGE)
-      .reprompt(HELP_REPROMPT)
-      .getResponse();
-  },
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            request.intent.name === 'AMAZON.HelpIntent';
+    },
+    handle(handlerInput)
+    {
+        return handlerInput.responseBuilder
+            .speak(HELP_MESSAGE)
+            .reprompt(HELP_REPROMPT)
+            .getResponse();
+    },
 };
 
 const ExitHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'IntentRequest'
-      && (request.intent.name === 'AMAZON.CancelIntent'
-        || request.intent.name === 'AMAZON.StopIntent');
-  },
-  handle(handlerInput) {
-    return handlerInput.responseBuilder
-      .speak(STOP_MESSAGE)
-      .getResponse();
-  },
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'IntentRequest' &&
+            (request.intent.name === 'AMAZON.CancelIntent' ||
+                request.intent.name === 'AMAZON.StopIntent');
+    },
+    handle(handlerInput)
+    {
+        return handlerInput.responseBuilder
+            .speak(STOP_MESSAGE)
+            .getResponse();
+    },
 };
 
 const SessionEndedRequestHandler = {
-  canHandle(handlerInput) {
-    const request = handlerInput.requestEnvelope.request;
-    return request.type === 'SessionEndedRequest';
-  },
-  handle(handlerInput) {
-    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+    canHandle(handlerInput)
+    {
+        const request = handlerInput.requestEnvelope.request;
+        return request.type === 'SessionEndedRequest';
+    },
+    handle(handlerInput)
+    {
+        console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
 
-    return handlerInput.responseBuilder.getResponse();
-  },
+        return handlerInput.responseBuilder.getResponse();
+    },
 };
 
 const ErrorHandler = {
-  canHandle() {
-    return true;
-  },
-  handle(handlerInput, error) {
-    console.log(`Error handled: ${error.message}`);
+    canHandle()
+    {
+        return true;
+    },
+    handle(handlerInput, error)
+    {
+        console.log(`Error handled: ${error.message}`);
 
-    return handlerInput.responseBuilder
-      .speak('Sorry, an error occurred.')
-      .reprompt('Sorry, an error occurred.')
-      .getResponse();
-  },
+        return handlerInput.responseBuilder
+            .speak('Sorry, an error occurred.')
+            .reprompt('Sorry, an error occurred.')
+            .getResponse();
+    },
 };
 
 const SKILL_NAME = 'Silly Number Facts';
@@ -89,37 +99,38 @@ const HELP_REPROMPT = 'What can I help you with?';
 const STOP_MESSAGE = 'Goodbye!';
 
 
-async function httpRequestPromise(options) {
-  //log the options in case we need to debug
-  console.log(`~~~~~~~~~~~~~~~${JSON.stringify(options)}~~~~~~~~~~~~~`);
-  // return new pending promise
-  return new Promise((resolve, reject) => {
-    const request = http.request(options, (response) => {
-      // reject the promise if the HTTP status isn't `Successful 2xx`
-      if (response.statusCode < 200 || response.statusCode > 299) {
-        reject(new Error('Failed to load page, status code: ' + response.statusCode));
-      }
-      // crete an array for holding data temporarily
-      const body = [];
-      // every time we get data, grab the chunk and push it to the array
-      response.on('data', (chunk) => body.push(chunk));
-      // when we are done, resolve the promise with all the chunks joined
-      response.on('end', () => resolve(body.join('')));
+async function httpRequestPromise(options)
+{
+    //log the options in case we need to debug
+    console.log(`~~~~~~~~~~~~~~~${JSON.stringify(options)}~~~~~~~~~~~~~`);
+    // return new pending promise
+    return new Promise((resolve, reject) =>
+    {
+        const request = http.request(options, (response) =>
+        {
+            // reject the promise if the HTTP status isn't `Successful 2xx`
+            if (response.statusCode < 200 || response.statusCode > 299)
+            {
+                reject(new Error('Failed to load page, status code: ' + response.statusCode));
+            }
+            // crete an array for holding data temporarily
+            const body = [];
+            // every time we get data, grab the chunk and push it to the array
+            response.on('data', (chunk) => body.push(chunk));
+            // when we are done, resolve the promise with all the chunks joined
+            response.on('end', () => resolve(body.join('')));
+        });
+        // reject the promise if there are connection errors of the request
+        request.on('error', (err) => reject(err));
+        request.end();
     });
-    // reject the promise if there are connection errors of the request
-    request.on('error', (err) => reject(err));
-    request.end();
-  });
 }
 
 const skillBuilder = Alexa.SkillBuilders.standard();
 
 exports.handler = skillBuilder
-  .addRequestHandlers(
-    GetNewFactHandler,
-    HelpHandler,
-    ExitHandler,
-    SessionEndedRequestHandler
-  )
-  .addErrorHandlers(ErrorHandler)
-  .lambda();
+    .addRequestHandlers(
+        GetNewFactHandler, HelpHandler, ExitHandler, SessionEndedRequestHandler
+    )
+    .addErrorHandlers(ErrorHandler)
+    .lambda();
